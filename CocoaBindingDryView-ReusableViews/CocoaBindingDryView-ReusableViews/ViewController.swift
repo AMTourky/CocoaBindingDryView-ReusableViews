065f8111775d7f6e9c5cf249dc3670d74c8a4043
AMTourky <abdelhameed.tourky@radius60.com>
2016-06-25 10:45:05 +0200

    init

---
+++ CocoaBindingDryView-ReusableViews/CocoaBindingDryView-ReusableViews/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CocoaBindingDryView-ReusableViews
+//
+//  Created by AMTourky on 6/25/16.
+//  Copyright © 2016 AMTourky. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
